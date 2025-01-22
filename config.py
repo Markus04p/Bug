3065e2e24beb4d3cc01774f07c5af8e5d4e697ce
Markus04p <teambotid04@gmail.com>
2025-01-22 20:55:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Marwin040/Vom",
+    "https://github.com/Markus04p/Bug",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
