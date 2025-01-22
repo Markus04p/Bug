3b18f8862d1856571a1d19141643b958e863a050
THE ARCHITECT <markusmarwin00@gmail.com>
2025-01-22 16:36:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = "27758016"
-API_HASH = "8d34cfffe27ab461eabbf0091b1a27df"
+API_ID = "12380656"
+API_HASH = "d927c13beaaf5110f25c505b7c071273"
 
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = "7697813860:AAGH_1x9zwCFJaF240elUe4S7MTVs4nII5g"
